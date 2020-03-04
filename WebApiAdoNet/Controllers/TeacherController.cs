e12e0331a6dfd2f32e56566f769b642f64eedb42
Boris Arturo González Rivera <borisgr04@gmail.com>
2020-03-03 21:43:56 -0500

    Agregar archivos de proyecto.

---
+++ WebApiAdoNet/Controllers/TeacherController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using WebApiAdoNet.Models;
+
+//https://hackernoon.com/asp-net-core-learn-crud-operations-in-ado-net-from-zero-to-hero-a0109ed2f8a4
+//https://medium.com/@didourebai/add-swagger-to-asp-net-core-3-0-web-api-874cb265854c
+//Install-Package Swashbuckle.AspNetCore -Version 5.0.0
+//https://github.com/domaindrivendev/Swashbuckle.AspNetCore
+
+
+namespace WebApiAdoNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        public TeacherController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // GET: api/Teacher
+        [HttpGet]
+        public IEnumerable<Teacher> Get()
+        {
+            List<Teacher> teacherList = new List<Teacher>();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"]; 
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "Select * From Teacher"; 
+                SqlCommand command = new SqlCommand(sql, connection);
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Teacher teacher = new Teacher();
+                        teacher.Id = Convert.ToInt32(dataReader["Id"]);
+                        teacher.Name = Convert.ToString(dataReader["Name"]);
+                        teacher.Skills = Convert.ToString(dataReader["Skills"]); 
+                        teacher.TotalStudents = Convert.ToInt32(dataReader["TotalStudents"]);
+                        teacher.Salary = Convert.ToDecimal(dataReader["Salary"]);
+                        teacher.AddedOn = Convert.ToDateTime(dataReader["AddedOn"]); 
+                        teacherList.Add(teacher);
+                    }
+                }
+                connection.Close();
+            }
+            return teacherList;
+        }
+
+        // GET: api/Teacher/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Teacher
+        [HttpPost]
+        public IActionResult Post(Teacher teacher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Insert Into Teacher (Name, Skills, TotalStudents, Salary) Values ('{teacher.Name}', '{teacher.Skills}','{teacher.TotalStudents}','{teacher.Salary}')";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                return Ok();
+            }
+
+
+        }
+
+        // PUT: api/Teacher/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Teacher teacher)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Update Teacher SET Name='{teacher.Name}', Skills='{teacher.Skills}', TotalStudents='{teacher.TotalStudents}', Salary='{teacher.Salary}' Where Id='{teacher.Id}'"; 
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            return Ok();
+        }
+
+        
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Delete From Teacher Where Id='{id}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        return BadRequest("Operation got error:" + ex.Message);
+                    }
+                    connection.Close();
+                }
+            }
+            return Ok();
+        }
+    }
+}
